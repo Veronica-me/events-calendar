d49da0ccb109b363af8d7bfc0d3af9f8b4c403b0
Veronica Mekhno <veronn.adams@gmail.com>
2023-11-30 11:47:59 +0100

    add:rerender planner after save form

--- src/scripts/main-scripts.js
+++ src/scripts/main-scripts.js
@@ -25,41 +25,56 @@ function validateForm() {
     }
     });
   // Function to handle form submission
-  function submitForm(event) {
-    event.preventDefault();
-      
+// Function to handle form submission
+function submitForm(event) {
+  event.preventDefault();
 
-    if (!validateForm()) {
+  if (!validateForm()) {
       return;
-    }
+  }
 
- 
-    const formData = {
+  const formData = {
       date: document.getElementById('eventDate').value,
       title: document.getElementById('eventTitle').value,
       time: document.getElementById('eventTime').value,
       place: document.getElementById('eventPlace').value,
       description: document.getElementById('eventDescription').value,
-    };
+  };
 
-   // console.log(formData);
-
-    $.ajax({
+  $.ajax({
       type: 'POST',
-      url: 'src/Controllers/EventController.php', 
+      url: 'src/Controllers/EventController.php',
       data: formData,
-      success: function(response) {
-        // Handle the server response (if needed)
-        console.log(response);
+      success: function (response) {
+          console.log(response);
+          clearForm();
+          
+          fetchUpdatedEvents();
       },
-      error: function(error) {
-        // Handle errors (if needed)
-        console.error(error);
+      error: function (error) {
+          console.error(error);
       }
-    });
+  });
+}
 
+// Function to fetch updated events and update the planner
+function fetchUpdatedEvents() {
+  fetch(`src/Controllers/EventsOutputController.php?action=getAllEvents`)
+      .then(response => response.json())
+      .then(data => {
+          eventsData = organizeEventsByDate(data.events || []);
+          updatePlanner();
+      })
+      .catch(error => console.error('Error fetching events:', error));
+}
 
-  }
+function clearForm() {
+  document.getElementById('eventDate').value = '';
+  document.getElementById('eventTitle').value = '';
+  document.getElementById('eventTime').value = '';
+  document.getElementById('eventPlace').value = '';
+  document.getElementById('eventDescription').value = '';
+}
 
 
 
@@ -68,3 +83,6 @@ function validateForm() {
     $('#datepicker').datepicker('setDate', new Date());
     
   });
+
+
+  
